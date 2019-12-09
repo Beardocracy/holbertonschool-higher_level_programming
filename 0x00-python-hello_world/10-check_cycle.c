f06292812d1d428edae9bd383977e3f225a7645e
Beardocracy <travisjbearden@gmail.com>
2019-12-09 22:44:05 +0000

    Fixed flaw where leader was not incrementing how I wanted

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,7 +2,7 @@
 
 /**
  * check_cycle - checks if a singly linked list has a cycle in it
- * list: pointer to first node of a linked list
+ * @list: pointer to first node of a linked list
  * Return: 0 if no cycle, 1 if there is a cycle
  */
 int check_cycle(listint_t *list)
@@ -13,10 +13,11 @@ int check_cycle(listint_t *list)
 
 	while (leader != NULL)
 	{
-		list = list->next;
-		if (list == NULL)
+		leader = leader->next;
+		if (leader == NULL)
 			return (0);
-		leader = leader->next->next;
+		leader = leader->next;
+		list = list->next;
 		if (list == leader)
 			return (1);
 	}
