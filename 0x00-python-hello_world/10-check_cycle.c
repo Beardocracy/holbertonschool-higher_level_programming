4af88e936707e7ad40d8ba26a5a99f99147fe002
Beardocracy <travisjbearden@gmail.com>
2019-12-09 22:29:06 +0000

    Added part 10 for testing

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * list: pointer to first node of a linked list
+ * Return: 0 if no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *leader;
+
+	if (list == NULL)
+		return (0);
+	leader = list;
+
+	while (leader != NULL)
+	{
+		list = list->next;
+		if (list == NULL)
+			return (0);
+		leader = leader->next->next;
+		if (list == leader)
+			return (1);
+	}
+
+	return (0);
+}
