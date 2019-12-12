bea6d925911287ca0fd499604368bee525eec671
Beardocracy <travisjbearden@gmail.com>
2019-12-12 21:34:20 +0000

    Added name guard

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-import sys
-args = len(sys.argv)
+if __name__ == "__main__":
+    import sys
+    args = len(sys.argv)
 
-print('{} argument{}'.format(args - 1, 's' if args != 2 else ''), end='')
-print('{}'.format('.' if args == 1 else ':'))
-i = 1
-for arg in sys.argv[1:]:
-    print('{}: {}'.format(i, arg))
-    i += 1
+    print('{} argument{}'.format(args - 1, 's' if args != 2 else ''), end='')
+    print('{}'.format('.' if args == 1 else ':'))
+    i = 1
+    for arg in sys.argv[1:]:
+        print('{}: {}'.format(i, arg))
+        i += 1
