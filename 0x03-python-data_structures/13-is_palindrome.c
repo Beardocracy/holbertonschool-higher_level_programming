d8bf5ce7a25693c28291ffc0f737cf7efc8fdc4d
Beardocracy <travisjbearden@gmail.com>
2019-12-18 04:57:15 +0000

    Removed many unnecessary loops

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,6 +1,5 @@
 #include "lists.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
  * is_palindrome - checks a list to see if it is a palindrome
@@ -10,21 +9,21 @@
 int is_palindrome(listint_t **head)
 {
 	listint_t *temp1, *temp2;
-	int start, len, i;
+	int start, len, i, end;
 
 	if (*head == NULL || (*head)->next == NULL)
 		return (1);
 	len = list_length(*head);
-	for (start = 0, len -= 1; start <= len; start++, len--)
+	temp1 = *head;
+	len -= 1;
+	for (start = 0, end = len; start <= end; start++, len -= 2, end--)
 	{
-		temp1 = *head;
-		temp2 = *head;
-		for (i = 0; i < start; i++)
-			temp1 = temp1->next;
+		temp2 = temp1;
 		for (i = 0; i < len; i++)
 			temp2 = temp2->next;
 		if (temp1->n != temp2->n)
 			return (0);
+		temp1 = temp1->next;
 	}
 	return (1);
 }
