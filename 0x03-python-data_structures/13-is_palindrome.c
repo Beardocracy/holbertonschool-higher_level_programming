e986da46bd4b3cc2e1dde24ab5eb37e871b3c810
Beardocracy <travisjbearden@gmail.com>
2019-12-18 05:08:40 +0000

    Added check for if list is a loop

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -13,7 +13,9 @@ int is_palindrome(listint_t **head)
 
 	if (*head == NULL || (*head)->next == NULL)
 		return (1);
-	len = list_length(*head);
+	if (loop_exists(head))
+		return (0);
+	len = list_length(head);
 	temp1 = *head;
 	len -= 1;
 	for (start = 0, end = len; start <= end; start++, len -= 2, end--)
@@ -33,9 +35,9 @@ int is_palindrome(listint_t **head)
  * @head: pointer to the head of the list.
  * Return: the number of nodes in the list.
  */
-int list_length(listint_t *head)
+int list_length(listint_t **head)
 {
-	listint_t *temp = head;
+	listint_t *temp = *head;
 	int count = 0;
 
 	while (temp != NULL)
@@ -44,4 +46,27 @@ int list_length(listint_t *head)
 		temp = temp->next;
 	}
 	return (count);
+}
+
+/**
+ * loop_exists - checks a listint_t list for a loop
+ * @head: pointer to the first node.
+ * Return: 1 if loop exists, 0 if not.
+ */
+int loop_exists(listint_t **head)
+{
+	listint_t *hare = *head;
+	listint_t *tort = *head;
+	
+	while (hare != NULL)
+	{
+		tort = tort->next;
+		hare = hare->next;
+		if (hare == NULL)
+			return (0);
+		hare = hare->next;
+		if (hare == tort)
+			return (1);
+	}
+	return (0);
 }
