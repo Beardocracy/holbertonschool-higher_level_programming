233eb11e0c3eef4b5eb57b36f517b30d6a686549
Beardocracy <travisjbearden@gmail.com>
2019-12-19 02:38:51 +0000

    Added the word the

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -12,7 +12,7 @@ void print_python_list_info(PyObject *p)
 
 	len = (int)PyList_Size(p);
 
-	printf("[*] Size of Python List = %d\n", len);
+	printf("[*] Size of the Python List = %d\n", len);
 	printf("[*] Allocated = %d\n", (int)list->allocated);
 	for (i = 0; i < len; i++)
 	{
