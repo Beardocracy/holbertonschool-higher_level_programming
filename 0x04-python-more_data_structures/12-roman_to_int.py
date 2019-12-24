80661fb110d64227658a0da4ba1fe62f5461ce26
Beardocracy <travisjbearden@gmail.com>
2019-12-24 02:54:42 +0000

    Fixed for case of string is None

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,8 +2,8 @@
 def roman_to_int(roman_string):
     romNums = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     total = 0
-    length = len(roman_string)
     if type(roman_string) == str:
+        length = len(roman_string)
         for n in range(length):
             if n == length - 1:
                 total += romNums[roman_string[n]]
