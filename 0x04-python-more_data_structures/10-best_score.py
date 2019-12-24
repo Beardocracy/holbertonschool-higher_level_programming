8e82f104936fceb1d7b10535ce8b70c8f8972ff0
Beardocracy <travisjbearden@gmail.com>
2019-12-24 02:11:57 +0000

    Added part 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,11 @@
 
 
 def best_score(a_dictionary):
-
+    bigKey = None
+    bigNum = 0
+    if type(a_dictionary) == dict:
+        for key in a_dictionary:
+            if a_dictionary[key] > bigNum:
+                bigNum = a_dictionary[key]
+                bigKey = key
+    return bigKey
