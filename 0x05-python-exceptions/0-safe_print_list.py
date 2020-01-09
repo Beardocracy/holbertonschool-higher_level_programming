68c7a2a6ab4afe7bcdedaae648d0ea364a80acdf
Beardocracy <travisjbearden@gmail.com>
2020-01-09 01:45:43 +0000

    Added part 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+
+    n = 0
+
+    for i in range(0, x):
+        try:
+            print(my_list[i], end='')
+        except IndexError:
+            break
+        n = n + 1
+
+    print()
+    return n
