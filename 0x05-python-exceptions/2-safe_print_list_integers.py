6ff3d8687a0415197239158ab51a7c476e633e20
Beardocracy <travisjbearden@gmail.com>
2020-01-09 02:22:51 +0000

    Added part 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for n in range(0, x):
+        try:
+            print("{:d}".format(my_list[n]), end='')
+        except (ValueError, TypeError):
+            continue
+        count = count + 1
+    print()
+    return count
