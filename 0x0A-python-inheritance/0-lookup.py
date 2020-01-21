2adb930db94c795868a44ddf3fcf787c24d87d4f
Beardocracy <travisjbearden@gmail.com>
2020-01-21 19:18:58 +0000

    Initial commit

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+This module contains a function
+'''
+
+
+def lookup(obj):
+    """
+    Returns the list of attributes and methods of an object
+    """
+    return dir(obj)
