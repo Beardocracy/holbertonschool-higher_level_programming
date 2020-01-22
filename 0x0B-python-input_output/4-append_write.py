abe24d47afa8e5a758a253eb298fb6f386a2f13c
Beardocracy <travisjbearden@gmail.com>
2020-01-22 01:26:21 +0000

    Added part 4

---
+++ 0x0B-python-input_output/4-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def append_write(filename="", text=""):
+    ''' appends a string at the end of a text file '''
+    with open(filename, 'a') as f:
+        char_written = f.write(text)
+    return char_written
