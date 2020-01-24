2ffb0e16b64b2a9bd0319c31425c2b7cde3414b6
Beardocracy <travisjbearden@gmail.com>
2020-01-24 23:08:16 +0000

    Added init file

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+''' Module for testing the Base class'''
+import unittest
+from models.base import Base
+
+class TestBase(unittest.TestCase):
+    
+    def setUp(self):
+        ''' Resets the class variable'''
+        Base._Base__nb_object = 0
+
+    def test_initialization(self):
+        new = Base()
+        self.assertEqual(type(new), Base)
+
+    def test_id_assignment(self):
+        a = Base()
+        self.assertEqual(a.id, 1)
+        b = Base(98)
+        self.assertEqual(b.id, 98)
+        c = Base()
+        self.assertEqual(c.id, 2)
