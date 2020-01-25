1f428f5983722c154370e1ea5bbc9b4d57b4e869
Beardocracy <travisjbearden@gmail.com>
2020-01-25 01:23:09 +0000

    Added documentation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-
+''' This module contains the class Base '''
 
 class Base:
-
+    ''' This is the definition for the base object '''
     __nb_objects = 0
-    
+
     def __init__(self, id=None):
+        ''' This is the first thing that runs when you create an instance '''
         if id is not None:
             self.id = id
         else:
