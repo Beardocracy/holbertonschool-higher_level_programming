1a204ec29d52ce6280657901b105f9df3c695245
Beardocracy <travisjbearden@gmail.com>
2020-01-26 23:09:39 +0000

    Added area method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+''' This module contains the class rectangle '''
 from models.base import Base
 
 class Rectangle(Base):
+    ''' This is the class rectangle which inherits from Base '''
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        ''' Class constructor '''
         self.width = width
         self.height = height
         self.x = x
@@ -12,10 +15,12 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        ''' Returns the width '''
         return self.__width
 
     @width.setter
     def width(self, width):
+        ''' Sets the width, with validation '''
         if type(width) is not int:
             raise TypeError('width must be an integer')
         elif width <= 0:
@@ -24,10 +29,12 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        ''' Returns the height '''
         return self.__height
 
     @height.setter
     def height(self, height):
+        ''' Sets the height, with validation '''
         if type(height) is not int:
             raise TypeError('height must be an integer')
         elif height <= 0:
@@ -36,10 +43,12 @@ class Rectangle(Base):
     
     @property
     def x(self):
+        ''' Gets the x '''
         return self.__x
 
     @x.setter
     def x(self, x):
+        ''' Sets the x, with validation '''
         if type(x) is not int:
             raise TypeError('x must be an integer')
         elif x < 0:
@@ -48,12 +57,18 @@ class Rectangle(Base):
     
     @property
     def y(self):
+        ''' Gets the y '''
         return self.__y
 
     @y.setter
     def y(self, y):
+        ''' Sets the y with validation '''    
         if type(y) is not int:
             raise TypeError('y must be an integer')
         elif y < 0:
             raise ValueError('y must be >= 0')
         self.__y = y
+
+    def area(self):
+        ''' Returns the area of the rectangle '''
+        return self.width * self.height
