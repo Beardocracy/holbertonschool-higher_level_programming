fc57fcb76dec5a0365db9c07579a1c26ad520262
Beardocracy <travisjbearden@gmail.com>
2020-01-27 21:34:34 +0000

    Added tests for save to and from json methods

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 ''' Module for testing the Base class'''
 import unittest
+import json
 from models.base import Base
 from models.rectangle import Rectangle
+
 
 class TestBase(unittest.TestCase):
     
@@ -31,3 +33,27 @@ class TestBase(unittest.TestCase):
         self.assertEqual(Base.to_json_string(None), "[]")
         empty = {}
         self.assertEqual(Base.to_json_string(empty), "[]")
+
+    def test_save_to_file(self):
+        ''' Tests the save to file method '''
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        Rectangle.save_to_file([r1, r2])
+        list_dicts_from_file = []
+        with open("Rectangle.json", "r") as f:
+            list_dicts_from_file = json.loads(f.read())
+        list_dicts = [r1.to_dictionary(), r2.to_dictionary()]
+        self.assertEqual(list_dicts, list_dicts_from_file)
+
+    def test_from_json(self):
+        ''' Tests the from_json_string method '''
+        list_input = [
+            {'id': 89, 'width': 10, 'height': 4}, 
+            {'id': 7, 'width': 1, 'height': 7}
+        ]
+        json_list_input = Rectangle.to_json_string(list_input)
+        list_output = Rectangle.from_json_string(json_list_input)
+        self.assertEqual(type(list_input), list)
+        self.assertEqual(type(json_list_input), str)
+        self.assertEqual(type(list_output), list)
+        self.assertEqual(list_output, list_input)
