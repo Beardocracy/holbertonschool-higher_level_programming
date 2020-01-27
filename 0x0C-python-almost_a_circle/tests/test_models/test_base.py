0da2b0dde15a9fcf3b9f94802590b1e98fa56ea1
Beardocracy <travisjbearden@gmail.com>
2020-01-27 19:32:26 +0000

    Added tests for to_json_string method

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -2,6 +2,7 @@
 ''' Module for testing the Base class'''
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
 
 class TestBase(unittest.TestCase):
     
@@ -20,3 +21,13 @@ class TestBase(unittest.TestCase):
         self.assertEqual(b.id, 98)
         c = Base()
         self.assertEqual(c.id, 2)
+
+    def test_to_json_string(self):
+        r1 = Rectangle(10, 7, 2, 8)
+        dictionary = r1.to_dictionary()
+        json_dictionary = Base.to_json_string([dictionary])
+        self.assertEqual(type(dictionary), dict)
+        self.assertEqual(type(json_dictionary), str)
+        self.assertEqual(Base.to_json_string(None), "[]")
+        empty = {}
+        self.assertEqual(Base.to_json_string(empty), "[]")
