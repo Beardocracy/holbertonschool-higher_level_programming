25c4a7f8df27817fa871a5568afb3b7c8204c5c4
Beardocracy <travisjbearden@gmail.com>
2020-03-04 01:50:22 +0000

    Added part 103

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,7 @@
+-- This shows the big table from ratings
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS ratings
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_show_genres.show_id
+GROUP BY tv_genres.name
+ORDER BY ratings DESC;
