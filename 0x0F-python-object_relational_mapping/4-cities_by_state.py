2355fca482553f0c93fd4f20994d384167f455d6
Beardocracy <travisjbearden@gmail.com>
2020-03-28 23:06:55 +0000

    Added if statement to make sure of correct number of arguments

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -5,18 +5,19 @@ import sys
 
 
 def show_states():
-    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name " +
-                "FROM states LEFT JOIN cities " +
-                "ON cities.state_id = states.id " +
-                "ORDER BY cities.id;")
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    db.close()
+    if len(sys.argv) == 4:
+        db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                             passwd=sys.argv[2], db=sys.argv[3])
+        cur = db.cursor()
+        cur.execute("SELECT cities.id, cities.name, states.name " +
+                    "FROM states LEFT JOIN cities " +
+                    "ON cities.state_id = states.id " +
+                    "ORDER BY cities.id;")
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            print(row)
+        cur.close()
+        db.close()
 
 if __name__ == '__main__':
     show_states()
