414e95c73b274ce97931ae47ccd19e189d93e4b8
Beardocracy <travisjbearden@gmail.com>
2020-03-28 21:42:39 +0000

    Added part 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+''' Lists all states from the database '''
+import MySQLdb
+import sys
+
+
+def show_states():
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        if row[1][0] == 'N':
+            print(row)
+    cur.close()
+    db.close()
+
+if __name__ == '__main__':
+    show_states()
