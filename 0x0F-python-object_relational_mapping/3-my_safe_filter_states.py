74d01404c23765c23de76ce28f1a28a688c45134
Beardocracy <travisjbearden@gmail.com>
2020-03-28 22:34:28 +0000

    Added part 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+''' Lists all states from the database that match condition. No injection '''
+import MySQLdb
+import sys
+
+
+def show_states():
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    if ';' not in sys.argv[4]:
+        cur.execute("SELECT * FROM states WHERE BINARY name = '{}'"
+                    .format(sys.argv[4]))
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            print(row)
+    cur.close()
+    db.close()
+
+if __name__ == '__main__':
+    show_states()
