360dac84b92afc9f7062c2bdedd6ee91a9246bd9
Beardocracy <travisjbearden@gmail.com>
2020-03-28 23:29:53 +0000

    Changed from join to where

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -10,8 +10,8 @@ def show_states():
                              passwd=sys.argv[2], db=sys.argv[3])
         cur = db.cursor()
         cur.execute("SELECT cities.id, cities.name, states.name " +
-                    "FROM states LEFT JOIN cities " +
-                    "ON cities.state_id = states.id " +
+                    "FROM cities, states " +
+                    "WHERE cities.state_id = states.id " +
                     "ORDER BY cities.id ASC;")
         query_rows = cur.fetchall()
         for row in query_rows:
