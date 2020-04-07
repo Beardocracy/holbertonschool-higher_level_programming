911875b72fc52c34ee173d063238dda4598efd63
Beardocracy <travisjbearden@gmail.com>
2020-04-07 22:09:56 +0000

    Added -s flag

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL, sends a POST request, and displays the body of the response
-curl -X POST "$1" -d 'email=hr@holbertonschool.com&subject=I will always be here for PLD'
+curl -s -X POST "$1" -d 'email=hr@holbertonschool.com&subject=I will always be here for PLD'
