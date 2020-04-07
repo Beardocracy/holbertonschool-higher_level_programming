ff0b5f25e5625248e906bf0d72c181233cab7a76
Beardocracy <travisjbearden@gmail.com>
2020-04-07 19:49:57 +0000

    Got rid of input check

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 # Takes in a URL, sends a request, displays size of the body
-
-if [[ $# -lt 1 ]]; then
-    echo "Usage: 0-body_size.sh URL"
-    exit
-fi
 curl -sI "$1" | sed -n '/Content-Length/ s/Content-Length: //p'
