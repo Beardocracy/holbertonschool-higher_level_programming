a596aa3e9ab4a3e0716c9be6ee9b26fde38d958a
Beardocracy <travisjbearden@gmail.com>
2020-04-13 20:49:24 +0000

    Fixed pep8 issues

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,11 +7,12 @@ import urllib.parse
 
 if __name__ == "__main__":
     values = {
-        'email' : sys.argv[2]
+        'email': sys.argv[2]
     }
     data = urllib.parse.urlencode(values)
     data = data.encode('ascii')
     req = urllib.request.Request(sys.argv[1], data)
+
     with urllib.request.urlopen(req) as response:
         content = response.read()
         print(content.decode('utf-8'))
