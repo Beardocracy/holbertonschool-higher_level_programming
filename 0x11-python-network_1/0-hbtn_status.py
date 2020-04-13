1fce22cdb1e29326c88eab656f3118136f4700fa
Beardocracy <travisjbearden@gmail.com>
2020-04-13 19:29:35 +0000

    Uncapitalized Response to response

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@ import urllib.request
 
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     content = response.read()
-    print("Body Response:")
+    print("Body response:")
     print("\t- type: {}".format(type(content)))
     print("\t- content: {}".format(content))
     print("\t- utf8 content: {}".format(content.decode('utf-8')))
