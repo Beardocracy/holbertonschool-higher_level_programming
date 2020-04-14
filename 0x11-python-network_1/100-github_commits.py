1a67c1ac4acb47a2a5074a52ea80973795afb88c
Beardocracy <travisjbearden@gmail.com>
2020-04-14 01:00:07 +0000

    Switched repo and user in url

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,8 +5,8 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    url = 'https://api.github.com/repos/{}/{}/commits'.format(sys.argv[1],
-                                                              sys.argv[2])
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(sys.argv[2],
+                                                              sys.argv[1])
     r = requests.get(url)
     com_list = r.json()
     com_dict = {}
