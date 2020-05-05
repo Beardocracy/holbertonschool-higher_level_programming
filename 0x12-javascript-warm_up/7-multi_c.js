9c4fa2d159fe4e71712a8f5d743e8bb696209cc1
Beardocracy <travisjbearden@gmail.com>
2020-05-05 21:56:59 +0000

    C is fun, not cool

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -7,6 +7,6 @@ if (isNaN(arg)) {
   console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < Number(process.argv[2]); i++) {
-    console.log('C is cool');
+    console.log('C is fun');
   }
 }
