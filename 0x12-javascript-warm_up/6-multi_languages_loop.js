92f06ba2c0383d5675bb900af03bd0d4dc84bbad
Beardocracy <travisjbearden@gmail.com>
2020-05-05 21:36:05 +0000

    Added part 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/*
+ * This script prints a few messages
+ */
+const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+array.forEach(function (entry) {
+  console.log(entry);
+});
