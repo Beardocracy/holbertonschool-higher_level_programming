11688017ee4a2e28b28ed4cf240a3700b13e577e
Beardocracy <travisjbearden@gmail.com>
2020-05-05 22:07:48 +0000

    Added part 9

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/*
+ * This script prints the addition of 2 integers
+ */
+const arg1 = Number(process.argv[2]);
+const arg2 = Number(process.argv[3]);
+const sum = arg1 + arg2;
+console.log(sum);
