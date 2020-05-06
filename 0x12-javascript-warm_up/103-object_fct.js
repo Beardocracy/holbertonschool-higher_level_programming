11a4dfbc2923ffb9d80d80912d701e2bc263444b
Beardocracy <travisjbearden@gmail.com>
2020-05-06 00:27:46 +0000

    Added part 103

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// Added a function to increment a value
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.incr = function () {
+  myObject.value++;
+};
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
