b0be8dcc566a6f080c7b3999cb07ceaabf6c71d9
Beardocracy <travisjbearden@gmail.com>
2020-05-15 15:38:50 -0700

    Added part 7

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,6 @@
+const character = $('div#character');
+$.get('https://swapi-api.hbtn.io/api/people/5/?format=json', (data, textStatus) => {
+  if (textStatus === 'success') {
+    character.text(data.name);
+  }
+});
